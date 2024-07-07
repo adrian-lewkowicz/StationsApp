0e5d062134c892666ec0c2d45a751a300d2163c9
Adrian Lewkowicz <adrian.lewkowicz.biala@gmail.com>
2024-07-07 10:50:18 +0200

    add room library and kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -43,6 +44,9 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation (libs.retrofit)
     implementation (libs.retrofit.converter.gson)
+    implementation(libs.room.runtime)
+    implementation(libs.room.compiler)
+    implementation(libs.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
